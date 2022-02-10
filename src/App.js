2b5a58d7291f7059a4e8d66d5263a625b6907c12
madhu <madhuchanda1716@gmail.com>
2022-02-10 13:52:59 +0530

    Added files

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Ganesh from "./Ganesh";
 import { Vinay } from './Vinay';
 import  Sourav  from './Sourav'; 
-
+import Madhu from './Madhu';
 function App() {
   return (
     <div className="App">
       <Ganesh/>
       <Vinay />
       <Sourav />
+      <Madhu />
     </div>
   );
 }
---
+++ src/Madhu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Madhu(){
+    return(
+        <div>
+            <h1>This is Madhu</h1>
+        </div>
+    )
+}
+export default Madhu;
