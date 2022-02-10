8e60d6590d7a6561acb80f677d4bc09e3b755d9f
Souravrout123 <iamsouravrout@gmail.com>
2022-02-10 07:24:59 +0530

    Sourav file was added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Ganesh from "./Ganesh";
 import { Vinay } from './Vinay';
+import  Sourav  from './Sourav'; 
 
 function App() {
   return (
     <div className="App">
       <Ganesh/>
       <Vinay />
+      <Sourav />
     </div>
   );
 }
---
+++ src/Sourav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sourav() {
+  return (
+    <div>
+        <h1>My name is Sourav</h1>
+    </div>
+  )
+}
+
+export default Sourav;
